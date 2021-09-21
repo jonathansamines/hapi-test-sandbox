c81aaa5b01c77f8c1a914515bc4725122ca038b1
Jonathan Samines <jn.samines@gmail.com>
2021-09-21 16:17:10 +0000

    Add test to verify initial settings value

---
+++ server-settings-initial-value-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const assert = require('assert');
+
+const server = Hapi.server();
+const serverWithInitialSettings = Hapi.server({ app: {} });
+
+assert.deepStrictEqual(serverWithInitialSettings.settings.app, {});
+assert.deepStrictEqual(server.settings.app, {});
